175a9c3581831efd71928094b8895ba6fcf6a8ba
Doux Salem <douxsalem@id.uff.br>
2021-05-09 12:43:24 -0300

    fix: creation d'une operation d'instanciation

--- src/main/java/com/salemdoux/ecole/EcoleApplication.java
+++ src/main/java/com/salemdoux/ecole/EcoleApplication.java
@@ -1,13 +1,31 @@
 package com.salemdoux.ecole;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EcoleApplication {
+import com.salemdoux.ecole.domain.Fonction;
+import com.salemdoux.ecole.repositories.FonctionRepository;
 
+@SpringBootApplication
+public class EcoleApplication implements CommandLineRunner{
+
+	@Autowired
+	private FonctionRepository fonctionRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(EcoleApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Fonction fonc1 = new Fonction(null, "Professeur");
+		Fonction fonc2 = new Fonction(null, "Surveillant");
+		
+		fonctionRepository.saveAll(Arrays.asList(fonc1, fonc2));
 	}
 
 }
